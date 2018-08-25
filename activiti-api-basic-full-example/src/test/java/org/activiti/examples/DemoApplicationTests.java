1bf71e70209010101982549831a64230403dbb1e
Abel L <abelli5@qq.com>
2018-08-25 21:12:13 +0800

    list process def.

--- activiti-api-basic-full-example/src/test/java/org/activiti/examples/DemoApplicationTests.java
+++ activiti-api-basic-full-example/src/test/java/org/activiti/examples/DemoApplicationTests.java
@@ -1,7 +1,14 @@
 package org.activiti.examples;
 
+import org.activiti.runtime.api.ProcessRuntime;
+import org.activiti.runtime.api.model.ProcessDefinition;
+import org.activiti.runtime.api.query.Page;
+import org.activiti.runtime.api.query.Pageable;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +16,24 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class DemoApplicationTests {
 
+    private Logger lgr = LoggerFactory.getLogger(DemoApplicationTests.class);
+    
+    @Autowired
+    private ProcessRuntime prt;
+    
+    @Autowired
+    private SecurityUtil securityUtil;
+
 	@Test
 	public void contextLoads() {
+	    securityUtil.logInAs("salaboy");
+	    
+        Page<ProcessDefinition> pdList = prt.processDefinitions(Pageable.of(0, 10));
+        
+        for (int i = 0; i < pdList.getContent().size(); i++) {
+            ProcessDefinition pd = pdList.getContent().get(i);
+            lgr.info("id:{}, name:{}, desc:{}", pd.getId(), pd.getName(), pd.getDescription());
+        }
 	}
 
 }
